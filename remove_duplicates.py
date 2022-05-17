6657ea1a6420ae64a6e8dc8cce7d6b78c69eaf8c
jarv88 <joseangel038@gmail.com>
2022-05-17 14:38:24 -0500

    remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -16,6 +16,16 @@ lista2 = [1,3,5,4,7,8,9,3,4,7,5,2,8,3,1,2,2]
 new_list2 = set()
 
 for i in range(len(lista2)):
-    new_list2.add(lista2[i])
+    new_list2.add(lista2[i]) # list(new_list2) -> para convertir a array
 
 print(new_list2)
+
+
+#forma super rapida con set
+lista3 = [1,3,5,4,7,8,9,3,4,7,5,2,8,3,1,2,2]
+
+new_list3 = set(lista3)
+
+new_list3 = list(new_list3) # para convertir a array
+print(new_list3)
+
